6317d664229809504f4a5322b625525b6a529f70
Suraj Pisal <surajpisal113@gmail.com>
2023-07-19 17:20:04 +0530

    setup static files in production

--- growgreen/settings.py
+++ growgreen/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/' 
-STATIC_ROOT = os.path.join(BASE_DIR, "/static")
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "app/static",
 
 ]
 MEDIA_URL = '/media/'
